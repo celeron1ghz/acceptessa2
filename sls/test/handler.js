5d5d29dfe4dde7f8c2892ee92b09faff769684e6
celeron1ghz <celeron1ghz@gmail.com>
2022-05-04 13:33:52 +0900

    impl basic queue

--- sls/test/handler.js
+++ sls/test/handler.js
@@ -1,6 +1,29 @@
 'use strict';
 
 module.exports.main = async (event) => {
+  if (!event.Records) {
+    throw new Error("no record");
+  }
+
+  for (const r of event.Records) {
+    let mess;
+    try {
+      mess = JSON.parse(r.body);
+    } catch (error) {
+      throw new Error("Error on queue parent: mess=" + error);
+    }
+
+    const id = mess.MessageId;
+    let data;
+
+    try {
+      data = JSON.parse(mess.Message);
+      console.log(data);
+    } catch (error) {
+      throw new Error("Error on queue child: id=" + id + " mess=" + error);
+    }
+  }
+
   console.log(JSON.stringify(event, null, 2));
   return null;
 };
